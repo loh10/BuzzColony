b6dd598a4f81023442bb2ee7cc8b655bb78d72c6
Loh10 <naturellement.toi@gmail.com>
2024-10-11 10:23:36 +0200

    Save and load map
    Menu And Save and load map

--- Assets/Script/MenuPrincipal/MenuManager.cs
+++ Assets/Script/MenuPrincipal/MenuManager.cs
@@ -3,26 +3,44 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-        public GameObject startMenu;
-        public string seedEnter;
+        [SerializeField]private GameObject startMenu,parameterMenu;
+        private string seedEnter;
+        private int seed;
+        
         public void Play()
         {
+                SaveAndLoad.Instance.SaveMap(seed);
+                SaveAndLoad.Instance.SaveGame();
                 SceneManager.LoadScene(1);
         }
 
+        public void LoadGame()
+        {
+              SceneManager.LoadScene(1);
+        }
+        
         public void Quit()
         {
                 Application.Quit();
         }
 
+        #region Open and Close Menu function
         public void OpenMenuStart()
         {
-                startMenu.SetActive(true);
+                startMenu.SetActive(!startMenu.activeSelf);
         }
-
+        
+        public void OpenMenuParameter()
+        {
+                parameterMenu.SetActive(!parameterMenu.activeSelf);
+        }
+        #endregion
+        
+        
         public void seedInput(string input)
         {
                 seedEnter = input;
+                SeedConverter seedConverter = new SeedConverter();
+                seed = seedConverter.SeedConvertion(seedEnter);
         }
-        
 }
